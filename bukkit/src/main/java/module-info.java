d18a5813bd867aaf48be3944a7c33095df88899d
H3xadecimal <h3xadecimal@afternode.cn>
2024-07-21 18:49:14 +0800

    fix: Fix package export

--- bukkit/src/main/java/module-info.java
+++ bukkit/src/main/java/module-info.java
@@ -8,4 +8,9 @@ module cn.afternode.commons.afternode.commons.bukkit.main {
     requires net.kyori.adventure.text.minimessage;
     requires net.kyori.examination.api;
     requires java.desktop;
+
+    exports cn.afternode.commons.bukkit;
+    exports cn.afternode.commons.bukkit.annotations;
+    exports cn.afternode.commons.bukkit.configurations;
+    exports cn.afternode.commons.bukkit.message;
 }
--- commons/src/main/java/module-info.java
+++ commons/src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module afternode.commons.commons.main {
     exports cn.afternode.commons;
-    exports cn.afternode.commons.reflect;
-    exports cn.afternode.commons.serialization;
+    exports cn.afternode.commons.binary;
     exports cn.afternode.commons.library;
     exports cn.afternode.commons.localizations;
+    exports cn.afternode.commons.reflect;
+    exports cn.afternode.commons.serialization;
 
     requires jdk.unsupported;
 }
