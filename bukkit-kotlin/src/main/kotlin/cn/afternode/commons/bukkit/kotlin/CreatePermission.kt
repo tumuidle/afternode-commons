f163a4ab7cbf72d06216506ed950f5525fb38a44
H3xadecimal <h3xadecimal@afternode.cn>
2024-07-31 11:14:47 +0800

    feat(bukkit-kotlin): Add createPermission

---
+++ bukkit-kotlin/src/main/kotlin/cn/afternode/commons/bukkit/kotlin/CreatePermission.kt
@@ -0,0 +1,36 @@
+package cn.afternode.commons.bukkit.kotlin
+
+import org.bukkit.Bukkit
+import org.bukkit.permissions.Permission
+import org.bukkit.permissions.PermissionDefault
+import kotlin.reflect.KProperty
+
+class CreatePermission(val node: String, val parent: Permission? = null, val default: PermissionDefault = PermissionDefault.OP, val onCreated: ((Permission) -> Unit)? = null) {
+    private val created by lazy { create() }
+
+    operator fun getValue(thisRef: CreatePermission, property: KProperty<*>
+    ): Permission {
+        return thisRef.created
+    }
+
+    private fun create(): Permission {
+        val realName = StringBuilder(parent?.name ?: "")
+        if (realName.isNotEmpty()) realName.append(".")
+        realName.append(node)
+        val perm = Permission(realName.toString())
+        Bukkit.getPluginManager().addPermission(perm)
+        onCreated?.invoke(perm)
+
+        return perm
+    }
+}
+
+/**
+ * Create a permission
+ * <br>
+ * ex. val permission by createPermission("plugin.permission")
+ * <br>
+ * @see CreatePermission
+ */
+fun createPermission(node: String, parent: Permission? = null, default: PermissionDefault = PermissionDefault.OP, onCreated: ((Permission) -> Unit)? = null) =
+    CreatePermission(node, parent, default, onCreated)
